62cfe7d5023f30fa01e6a33a0cdce8755fbf5ef1
Yasmin <ym222bs@student.lnu.se>
2018-09-26 13:25:00 +0200

    added user validationmessages

--- model/User.cs
+++ model/User.cs
@@ -5,37 +5,49 @@ namespace jolly_pirate
     class User
     {
         private string _socialSecurityNumber;
-        private string _password;
-
+        private string _fullName;
         private int _id;
 
-        public User(string socialSecurityNumber, string password, int id)
+        // TODO: Have some type of way to Collect boats here.
+        // TODO: Remove/ Add method as well.
+
+        public User(string socialSecurityNumber, string fullName, int id)
         {
             this._socialSecurityNumber = socialSecurityNumber;
-            this._password = password;
+            this._fullName = fullName;
             this._id = id;
         }
 
-        private void setSocialSecurityNumber()
-        {
-            throw new Exception("Not yet implemented");
-        }
 /// i see dead people. 
-        private void setPassword()
-        {
-            throw new Exception("Not yet implemented");
-        }
 
         private void setId()
         {
             throw new Exception("Not yet implemented");
         }
 
-        public string getSocialSecurityNumber()
+        public string SocialSecurityNumber
         {
-            return this._socialSecurityNumber;
+           get { return this._socialSecurityNumber; }
+           set 
+           { 
+               if(String.IsNullOrWhiteSpace(value) || value.Length != 10) 
+               {
+                   throw new Exception("Not valid social secirity number, enter 10 digits.");
+               }
+           }
         }
-        
+         public string Name
+        {
+           get { return this._fullName; }
+           set 
+           {
+               if(String.IsNullOrWhiteSpace(value)) 
+               {
+                   throw new Exception("No name given. Please enter a full name.");
+               }
+               _fullName = value;
+            }
+        } 
 
     }
 }
