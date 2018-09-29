9e4baf910c1d3ba3aa72630472453160bf4ae8f1
Yasmin <ym222bs@student.lnu.se>
2018-09-29 20:48:00 +0200

    buggfix

--- controller/Controller.cs
+++ controller/Controller.cs
@@ -15,79 +15,73 @@ namespace jolly_pirate {
         public void InitMasterController () {
             UserDAL userDAL = new UserDAL ();
             View view = new View ();
-            Console.WriteLine(this.findUserByID(1));
 
+            do {
+                Console.Clear ();
+                view.StartMenu ();
+                int input;
 
-            // this.selectAUser (1);
+                try {
+                    if (int.TryParse (Console.ReadLine (), out input) && input >= 0 && input <= 2) {
+                        switch (input) {
+                            case 0: 
+                                     Console.WriteLine (); 
+                                     return;
+                            case 1:
+                                RegisterModel registerModel = new RegisterModel(userDAL, view);
+                                registerModel.TryRegister(view.RegNumber(), view.RegFullName());
+                                // this.TryRegister (view.RegNumber (), view.RegFullName ());
+                                break;
 
-        //     do {
-        //         Console.Clear ();
-        //         view.StartMenu ();
-        //         int input;
+                            case 2:
+                                /// BoatAssignment
+                                this.findUserByID(view.SelectMemberWirthID());
+                                /// Call login method from LonginController
+                                break;
 
-        //         try {
-        //             if (int.TryParse (Console.ReadLine (), out input) && input >= 0 && input <= 2) {
-        //                 switch (input) {
-        //                     case 0: 
-        //                              Console.WriteLine (); 
-        //                              return;
-        //                     case 1:
-        //                         RegisterModel registerModel = new RegisterModel(userDAL, view);
-        //                         registerModel.TryRegister(view.RegNumber(), view.RegFullName());
-        //                         // this.TryRegister (view.RegNumber (), view.RegFullName ());
-        //                         break;
+                            case 3:
 
-        //                     case 2:
-        //                         /// BoatAssignment
-        //                         this.selectAUser(view.SelectMemberWirthID());
-        //                         /// Call login method from LonginController
-        //                         break;
+                                Console.WriteLine ("Case: {0}", input);
+                                // Call login method from LonginController
+                                break;
+                            case 4:
 
-        //                     case 3:
-
-        //                         Console.WriteLine ("Case: {0}", input);
-        //                         // Call login method from LonginController
-        //                         break;
-        //                     case 4:
-
-        //                         Console.WriteLine ("Case: {0}", input);
-        //                         // Call login method from LonginController
-        //                         break;
-        //                 }
-        //             } else {
-        //                 Console.BackgroundColor = ConsoleColor.Red;
-        //                 Console.ForegroundColor = ConsoleColor.White;
-        //                 Console.WriteLine ("You need to enter a number between 0 and 2!\nPress any key to continue, ESC exits ");
-        //                 Console.ResetColor();
-        //             }
-        //         } catch (Exception e) {
-        //             Console.WriteLine ("{0} Exception caught.", e);
-        //         }
-        //     }
-        //     while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-        // }
+                                Console.WriteLine ("Case: {0}", input);
+                                // Call login method from LonginController
+                                break;
+                        }
+                    } else {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine ("You need to enter a number between 0 and 2!\nPress any key to continue, ESC exits ");
+                        Console.ResetColor();
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine ("{0} Exception caught.", e);
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
 
         // public void TryRegister (string number, string name) {
         //     try {
         //         // Console.Write(numberOfDigits);
-
         //         if (number.Length != 10) {
         //             throw new Exception ("Social number must contain 10 digits!");
         //         }
-
         //         if (name.Length < 3) {
         //             throw new Exception ("Full name must have atleast 3 characters");
         //         } else {
         //             User user = new User (number, name, GenerateID ());
-
-        //             this.userDAL.addToFile (user);
+        //             this.userDAL.addUser(user);
+        //             this.userDAL.saveToFile();
         //             this.view.RegSuccess ();
 
         //         }
         //     } catch (Exception e) {
         //         Console.WriteLine (e.Message);
         //     }
-        }
+        // }
 
         private int GenerateID () {
             if (this.userDAL.userInfo.Count == 0) {
